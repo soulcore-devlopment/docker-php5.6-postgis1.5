eb234795938db8487ca6b20d1e249f9ad1db695c
guilherme.prado@soulcore.com.br <guilherme.prado@soulcore.com.br>
2019-09-13 09:58:30 -0300

    centos6 postgres-9.1-pgis-1.5
    Still with bug on stop/start - fix in https://github.com/soulcore-devlopment/postgresql-9.1-postgis-1.5.git at docker-entrypoint.sh
    <code>
      if [ ! $(ls -a "${PG_DATA}/wasrun") ]; then 
      ...
      cat /dev/null > "${PG_DATA}/wasrun";
      chown postgres:postgres "${PG_DATA}/wasrun";
      chmod 755 "${PG_DATA}/wasrun";
    </code>
    
    Version: https://i.imgur.com/1dKdzAT.png

--- postgresql/run-postgresql.sh
+++ postgresql/run-postgresql.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-mkdir -p ${POSTGRES_DATA_FOLDER}
-chown postgres:postgres ${POSTGRES_DATA_FOLDER}
-chmod 700 ${POSTGRES_DATA_FOLDER}
+set -e
+if [ "$1" = 'postgres' ]; then
 
-# Check if data folder is empty. If it is, start the dataserver
-if ! ["$(ls -A ${POSTGRES_DATA_FOLDER})" ]; then
-    su postgres -c "/usr/pgsql-9.0/bin/initdb  --encoding=${ENCODING} --locale=${LOCALE}.${ENCODING} --lc-collate=${LOCALE}.${ENCODING}  --lc-monetary=${LOCALE}.${ENCODING}  --lc-numeric=${LOCALE}.${ENCODING}  --lc-time=${LOCALE}.${ENCODING}  -D ${POSTGRES_DATA_FOLDER}"
+    mkdir -p ${POSTGRES_DATA_FOLDER}
+    chown postgres:postgres ${POSTGRES_DATA_FOLDER}
+    chmod 700 ${POSTGRES_DATA_FOLDER}
 
-    # Modify basic configutarion
-    su postgres -c "echo \"host all all 0.0.0.0/0 md5\" >> $POSTGRES_DATA_FOLDER/pg_hba.conf"
-    su postgres -c "echo \"host all all ::0/0 md5\" >> $POSTGRES_DATA_FOLDER/pg_hba.conf"
-    su postgres -c "echo \"listen_addresses='*'\" >> $POSTGRES_DATA_FOLDER/postgresql.conf"
+    # Check if data folder is empty. If it is, start the dataserver
+    if ! [ -z "$(ls -A ${POSTGRES_DATA_FOLDER})" ]; then
+        su postgres -c "/usr/pgsql-9.1/bin/initdb  --encoding=${ENCODING} --locale=${LOCALE}.${ENCODING} --lc-collate=${LOCALE}.${ENCODING}  --lc-monetary=${LOCALE}.${ENCODING}  --lc-numeric=${LOCALE}.${ENCODING}  --lc-time=${LOCALE}.${ENCODING}  -D ${POSTGRES_DATA_FOLDER}"
 
-    # Establish postgres user password and run the database
-    su postgres -c "/usr/pgsql-9.0/bin/pg_ctl -w -D ${POSTGRES_DATA_FOLDER} start"
-    su postgres -c "/usr/pgsql-9.0/bin/psql -h localhost -U postgres -p 5432 -c \"alter role postgres password '${POSTGRES_PASSWD}';\""
-    su postgres -c "/usr/pgsql-9.0/bin/pg_ctl -w -D ${POSTGRES_DATA_FOLDER} stop"
+        # Modify basic configutarion
+        su postgres -c "echo \"host all all 0.0.0.0/0 md5\" >> $POSTGRES_DATA_FOLDER/pg_hba.conf"
+        su postgres -c "echo \"host all all ::0/0 md5\" >> $POSTGRES_DATA_FOLDER/pg_hba.conf"
+        su postgres -c "echo \"listen_addresses='*'\" >> $POSTGRES_DATA_FOLDER/postgresql.conf"
+
+        # Establish postgres user password and run the database
+        su postgres -c "/usr/pgsql-9.1/bin/pg_ctl -w -D ${POSTGRES_DATA_FOLDER} start"
+        su postgres -c "/usr/pgsql-9.1/bin/psql -h localhost -U postgres -p 5432 -c \"alter role postgres password '${POSTGRES_PASSWD}';\""
+        su postgres -c "/usr/pgsql-9.1/bin/pg_ctl -w -D ${POSTGRES_DATA_FOLDER} stop"
+    fi
+
+    echo "Start postgres"
+    # Start the database
+    su postgres -c "/usr/pgsql-9.1/bin/postgres -D $POSTGRES_DATA_FOLDER"
+    
 fi
-
-echo "Start postgres"
-# Start the database
-su postgres -c "/usr/pgsql-9.0/bin/postgres -D $POSTGRES_DATA_FOLDER"
